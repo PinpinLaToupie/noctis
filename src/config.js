cc398fa49bfd40100bfed88391501b34e839eacb
Clément Féménias--Noir <femenias.c@gmail.com>
2026-04-15 23:11:33 +0200

    v2.2.5 v2

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // (e.g., GitHub Releases, Dropbox, Google Drive direct link)
 export const APK_URL = 'https://github.com/PinpinLaToupie/noctis/releases/download/v2.2.5/noctisV2.2.5.apk'
 export const APP_VERSION = '2.2.5'
-export const APK_SIZE = '162 MB'
+export const APK_SIZE = '149 MB'
